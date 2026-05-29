9e3ce386e7c227b8cd9f5544ad8d5213994414eb
Rasik Awasthi <rishucent@gmail.com>
2026-05-29 11:29:59 +0530

    Admin

--- natures_academy/settings.py
+++ natures_academy/settings.py
@@ -78,9 +78,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join (BASE_DIR, 'media') # a different directory from 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join (BASE_DIR, 'static'), # the 'static' directory at the project root
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # RAG settings
